8103e838f49d6af1da9f8d4272ba6bcc7adb1861
BlancJH <abcdst@naver.com>
2024-11-21 11:19:07 +0800

    Create data processor for cluster bin

---
+++ src/data_processing.py
@@ -0,0 +1,22 @@
+from sklearn.cluster import KMeans
+
+class DataProcessor():
+
+    @staticmethod
+    def k_means_cluster_bin(df, feature, cluster_number, random_state=42):
+
+
+        # Reshape data for KMeans
+        feature_values = df[feature].values.reshape(-1, 1)
+
+        # Apply KMeans clustering
+        kmeans = KMeans(n_clusters=cluster_number, random_state=random_state)
+        df[f'{feature}_cluster'] = kmeans.fit_predict(feature_values)
+
+        # Generate cluster labels dynamically based on the cluster_number
+        cluster_labels = {i: f"Cluster {i + 1}" for i in range(cluster_number)}
+
+        # Assign cluster labels based on K-means
+        df[f'{feature}_binned'] = df[f'{feature}_cluster'].map(cluster_labels)
+        
+        return df
